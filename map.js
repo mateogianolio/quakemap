bf9299389a5299a461dc4a8e8297ea9d1c18d873
mateogianolio <gianoliomateo@gmail.com>
2016-02-08 20:32:39 +0100

    now we're getting somewhere

--- map.js
+++ map.js
@@ -1,103 +1,63 @@
 (function () {
   'use strict';
 
-  mapboxgl.accessToken = 'pk.eyJ1IjoibWF0ZW9naWFub2xpbyIsImEiOiJjaWs2MzRrcmMwMDRndnJrc2tibTZmeW8xIn0.-HNhp-sfXXy3DnCdgsNtpQ';
-  var map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: window.mapStyle, //stylesheet location
-    center: [45, 45], // starting position
-    zoom: 0 // starting zoom
-  });
+  L.mapbox.accessToken = 'pk.eyJ1IjoibWF0ZW9naWFub2xpbyIsImEiOiJjaWs2MzRrcmMwMDRndnJrc2tibTZmeW8xIn0.-HNhp-sfXXy3DnCdgsNtpQ';
+  var map = L.mapbox
+    .map('map', 'mateogianolio.64d36a05')
+    .setView([0, 0], 3);
 
-  var popup = new mapboxgl.Popup({
-    closeButton: false,
-    closeOnClick: false
-  });
+  map.zoomControl.removeFrom(map);
 
-  map.on('load', function () {
-    var date = window.location.search.replace('?', '&');
-    map.addSource('quakes', {
-      type: 'geojson',
-      data: 'http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson' + date
-    });
-
-    var colors = ['green', 'yellow', 'orange', 'red'],
-        options,
-        magnitude,
-        i;
-
-    for (i = 0; i < colors.length; i++) {
-      magnitude = i * 2.5;
-
-      options = { type: 'circle', source: 'quakes' };
-      options.id = 'quake-' + i;
-      options.interactive = true;
-      options.filter = [
-        'all',
-        ['>=', 'mag', magnitude],
-        ['<', 'mag', magnitude + 2.5]
-      ];
-      options.paint = {
-        'circle-radius': magnitude < 1 ? 1.5 : 1.5 * magnitude,
-        'circle-color': colors[i],
-        'circle-opacity': magnitude < 1 ? 0.1 : magnitude / 10,
-      };
-      map.addLayer(options);
-    }
-
-    for (i = 0; i < colors.length; i++) {
-      options = { type: 'circle', source: 'quakes' };
-      options.id = 'quake-alert-' + i;
-      options.filter = [
-        'all',
-        ['==', 'alert', colors[i]]
-      ];
-      options.paint = {
-        'circle-radius': 0,
-        'circle-color': colors[i],
-        'circle-opacity': 0
-      };
-      map.addLayer(options);
-    }
-
-    function animate(time) {
-      var f = Math.sin,
-          abs = Math.abs,
-          t = time / 1000,
-          i;
-
-      if (Math.cos(t) < 0)
-        t += Math.PI;
-
-      for (i = 0; i < colors.length; i++) {
-        map.setPaintProperty('quake-alert-' + i, 'circle-radius', (i + 1) * 5 * (1 + f(t)));
-        map.setPaintProperty('quake-alert-' + i, 'circle-opacity', 1 - f(t));
+  window.heat = L
+    .heatLayer([], {
+      maxZoom: 3,
+      radius: 3,
+      blur: 6,
+      gradient: {
+        '0': 'green',
+        '0.6': 'yellow',
+        '0.9': 'red'
       }
+    })
+    .addTo(map);
 
-      requestAnimationFrame(animate);
-    }
+  var max = 600,
+      i = 0;
 
-    animate(0);
+  window.pause = true;
+  window.traverse = function(start) {
+    if (window.pause || start.getTime() > Date.now())
+      return;
 
-    map.on('mousemove', function (event) {
-      map.featuresAt(event.point, {
-        radius: 10,
-        includeGeometry: true,
-        layer: ['quake-0', 'quake-1', 'quake-2', 'quake-3']
-      }, function (error, features) {
-        map.getCanvas().style.cursor = (!error && features.length) ? 'pointer' : '';
+    var month = 1000 * 60 * 60 * 24 * 30;
+    var end = new Date(start.getTime() + month);
 
-        if (error || !features.length) {
-          popup.remove();
-          return;
-        }
+    var url = 'http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson';
+    url += '&starttime=';
+    url += start.getFullYear() + '-' + (start.getMonth() + 1) + '-' + start.getDate();
+    url += '&endtime=';
+    url += end.getFullYear() + '-' + (end.getMonth() + 1) + '-' + end.getDate();
 
-        var feature = features[0];
-        popup
-          .setLngLat(feature.geometry.coordinates)
-          .setHTML('Magnitude: <b>' + feature.properties.mag + '</b><br>Date: <b>' + new Date(feature.properties.time).toLocaleDateString() + '</b>')
-          .addTo(map);
+    console.log(url);
+
+    var layer = L.mapbox
+      .featureLayer()
+      .loadURL(url)
+      .on('ready', function () {
+        layer.eachLayer(function (l) {
+          var data = l.getLatLng();
+          data.alt = l.feature.properties.mag / 10.0;
+
+          window.heat.addLatLng(data);
+
+          var time = new Date(l.feature.properties.time);
+          var months = ['January', 'February', 'March', 'April', 'May', 'June',
+                        'July', 'August', 'September', 'October', 'November',
+                        'December'];
+          document.getElementById('time').innerHTML = months[time.getMonth()] + ' ' + time.getFullYear();
+        });
+
+        setTimeout(traverse.bind(null, end), 500);
       });
-    });
-  });
+  };
 }());
